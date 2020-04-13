ff8fc694a719ec339eafb95fc2cd9bd080c9ad38
KrystianGrabowski <krystian987543@gmail.com>
2020-04-13 18:05:50 +0200

    Add toggler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Do the laundry',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Team meeting',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Go for a walk',
+        completed: false
+      }
+    ]
+  }
+
+  // Toggler
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map( todo => {
+      if (todo.id === id ) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) });
+  }
+
+  render() {
+     return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todoitem.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+
+export class Todoitem extends Component {
+  getStyle = () => {
+    return {
+      background: '#e4e4e4',
+      padding: '1rem',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+
+  markComplete = () => {
+    console.log(this.props);
+  }
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={ this.getStyle() }>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {'  '}
+          { title }
+          <button style={btnStyle}>x</button>
+        </p>
+      </div>
+    )
+  }
+}
+
+const btnStyle = {
+  background: '#cc4444',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 10px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
+}
+
+// const itemStyle = {
+//   backgroundColor: '#e6e6e6'
+// }
+
+Todoitem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
+
+export default Todoitem
